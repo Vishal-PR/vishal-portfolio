509ba26977899304c969f28e5121df5977962066
Vishal-PR <vishalpippirishetti@gmail.com>
2023-02-25 23:21:22 +0530

    Added instagram project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import WeatherApp from "../assets/weatherApp.png";
+import InstagramUi from "../assets/instagram ui clone.png"
 
 function Portfolio() {
   const portfolios = [
     {
       id: 1,
       src: WeatherApp,
-      href: "https://rainbow-zabaione-d6bf7c.netlify.app/"
+      href: "https://rainbow-zabaione-d6bf7c.netlify.app/",
+      projectName: "Weather App"
     },
+    {
+      id:2,
+      src: InstagramUi,
+      href:"https://beamish-dodol-e511bb.netlify.app/",
+      projectName: "Instagram Ui"
+    }
     
   ];
 
@@ -35,13 +43,13 @@ function Portfolio() {
         </div>
 
         <div  className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-        {portfolios.map(({id,src}) => (
+        {portfolios.map(({id,src,projectName}) => (
             <div key={id}>
             <div className="shadow-md shadow-gray-600 rounded-lg ">
               <img
                 src={src}
                 alt=""
-                className="rounded-md duration-200 hover:scale-105"
+                className="rounded-md duration-200 hover:scale-105 w-[800px] h-[150px]"
               />
               <div className="flex items-center justify-center ">
                 <button onClick={()=>handleDemoProject(id)} className="w-1/2 px-6 -py-3 m-4 duration-200 hover:scale-110">
@@ -53,7 +61,7 @@ function Portfolio() {
               </div>  
             </div>
             <div className="flex items-center justify-center mt-4 ">
-                    <h2 className="text-2xl">Weather App </h2>
+                    <h2 className="text-2xl">{projectName}</h2>
             </div>
           </div>
         ))}
